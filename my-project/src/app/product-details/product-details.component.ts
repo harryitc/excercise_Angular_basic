6d365628cdd2841d54f00c27b078dfcfce93d641
Phan Ngọc Cường <cuongharryit@gmail.com>
2022-12-26 15:59:02 +0700

    Adding - Navigation - View Product details - Routing - Them Huong dan

--- my-project/src/app/product-details/product-details.component.ts
+++ my-project/src/app/product-details/product-details.component.ts
@@ -12,9 +12,12 @@ export class ProductDetailsComponent implements OnInit {
   constructor(private route:ActivatedRoute) { }
   ngOnInit(): void{
     // First get the product id from the current route.
+    // đầu tiên, lấy route hiện tại mà chúng ta đang nhập URL
     const routeParams = this.route.snapshot.paramMap
+    // lấy id của route mà chúng ta đã lấy được cái route đó.
     const productIdFromRoute = Number(routeParams.get('productID'))
   // Find the product that correspond with the id provided in route.
+  // so sánh 
   this.product = products.find(product => product.id === productIdFromRoute)
   }
 }
